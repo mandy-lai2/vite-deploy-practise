724fce849eb99d06deefb15220447b3a019e3351
DESKTOP-V8E7JBJ\Mandy <chjo2980@gmail.com>
2023-01-31 21:00:30 +0800

    vite-deploy

---
+++ src/views/AboutView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ data }}
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+<script>
+export default {
+  data() {
+    return {
+      data: {},
+    };
+  },
+  mounted() {
+    this.axios.get("https://randomuser.me/api/").then((res) => {
+      console.log(res.data);
+      this.data = res.data.results[0];
+    });
+  },
+};
+</script>
